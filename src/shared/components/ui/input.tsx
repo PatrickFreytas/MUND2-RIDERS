090384e07cf11dbf2d7f1fe265b878697f6b5a69
PatrickFreytas <135546208+PatrickFreytas@users.noreply.github.com>
2025-08-26 23:52:33 -0500

    feat: add components for UI

---
+++ src/shared/components/ui/input.tsx
@@ -0,0 +1,72 @@
+import * as React from "react";
+import { cn, formatPrice } from "@/lib/utils";
+import {useEffect, useReducer} from "react";
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Input = React.forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLInputElement>>(
+  ({ className, type, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={cn(
+          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+          className,
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  },
+);
+Input.displayName = "Input";
+
+const MoneyInput = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, onChange, disabled, ...props }, ref) => {
+    const initialValue = props.value ? formatPrice(Number(props.value)) : "";
+
+    const [value, setValue] = useReducer((_: any, next: string) => {
+      const digits = next.replace(/\D/g, "");
+      return formatPrice(Number(digits) / 100);
+    }, initialValue);
+
+    useEffect(() => {
+      setValue(formatPrice(Number(props.value)) || "");
+    }, [props.value]);
+
+    function handleChange(
+      realChangeFn: React.ChangeEventHandler<HTMLInputElement> | undefined,
+      ev: React.ChangeEvent<HTMLInputElement>,
+    ) {
+      const formattedValue = ev.target.value;
+      const digits = formattedValue.replace(/\D/g, "");
+      const realValue = Number(digits) / 100;
+      if (realChangeFn)
+        realChangeFn({
+          ...ev,
+          target: { ...ev.target, value: realValue.toString() },
+        });
+    }
+    return (
+      <input
+        className={cn(
+          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+          className,
+        )}
+        placeholder={props.placeholder}
+        type="string"
+        onChange={(ev) => {
+          setValue(ev.target.value);
+          handleChange(onChange, ev);
+        }}
+        value={value}
+        ref={ref}
+        disabled={disabled}
+      />
+    );
+  },
+);
+MoneyInput.displayName = "MoneyInput";
+
+export { Input, MoneyInput };
---
+++ src/shared/user-auth-form.tsx
@@ -0,0 +1,130 @@
+"use client";
+
+import { Button } from "@/shared/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/shared/components/ui/form";
+import { Input } from "@/shared/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createUser } from "@/user/actions";
+import { useSearchParams } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import { signIn } from "next-auth/react";
+import * as z from "zod";
+// import GoogleSignInButton from "../github-auth-button";
+
+const formSchema = z.object({
+  email: z.string().email({ message: "Ingrese un email válido" }),
+  password: z
+    .string()
+    .min(6, { message: "La contraseña debe tener al menos 6 caracteres" }),
+});
+
+type UserFormValue = z.infer<typeof formSchema>;
+interface UserAuthFormProps {
+  action: "login" | "signup";
+}
+
+export default function UserAuthForm({ action }: UserAuthFormProps) {
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get("callbackUrl");
+  const [loading, setLoading] = useState(false);
+  const defaultValues = {
+    email: "",
+    password: "",
+  };
+  const form = useForm<UserFormValue>({
+    resolver: zodResolver(formSchema),
+    defaultValues,
+  });
+  const router = useRouter();
+
+  const onSubmit = async (data: UserFormValue) => {
+    if (action === "signup") {
+      const createUserResponse = await createUser(
+        "TODO: This view is not beging used, do we need to remove this?",
+        data.email,
+        data.password,
+      );
+      if (!createUserResponse.success) {
+        form.setError("email", {
+          type: "manual",
+          message: createUserResponse.message,
+        });
+      }
+
+      const signInResponse = await signIn("credentials", {
+        email: data.email,
+        password: data.password,
+      });
+
+      console.log({ signInResponse });
+    } else {
+      await signIn("credentials", {
+        email: data.email,
+        password: data.password,
+        redirect: false,
+      });
+      window.location.href = callbackUrl || window.location.origin;
+    }
+  };
+
+  return (
+    <>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-2 w-full"
+        >
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    type="email"
+                    placeholder="Ingresa tu email"
+                    disabled={loading}
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Contraseña</FormLabel>
+                <FormControl>
+                  <Input type="password" disabled={loading} {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button
+            disabled={loading}
+            className="ml-auto w-full mt-6"
+            type="submit"
+          >
+            {action === "login" ? "Iniciar sesión" : "Registrarse"}
+          </Button>
+        </form>
+      </Form>
+      {/*<GoogleSignInButton />*/}
+    </>
+  );
+}

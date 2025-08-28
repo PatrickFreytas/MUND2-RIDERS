2f026ed77d3b0d2a45bebd595678f63f347026db
PatrickFreytas <135546208+PatrickFreytas@users.noreply.github.com>
2025-08-27 22:24:08 -0500

    feat: add product-cart component

---
+++ src/product/components/product-card.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import {Input} from "@/shared/components/ui/input";
+
+export default function ProductCard(){
+
+  return (
+    <div>
+      <Input className="">
+
+      </Input>
+    </div>
+  )
+}

27497a494395130b332b841bd79f3964c75f4284
PatrickFreytas <135546208+PatrickFreytas@users.noreply.github.com>
2025-08-28 09:58:22 -0500

    feat: add constant entity

---
+++ src/constants/data.ts
@@ -0,0 +1,10 @@
+import { NavItem } from "@/ui/types";
+
+export const navItems: NavItem[] = [
+  {
+    title: "Productos",
+    href: "/dashboard/products",
+    icon: "blocks",
+    label: "productos",
+  },
+];

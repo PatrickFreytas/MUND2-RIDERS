60ae73f3037304d1d19b0c33ca324665eef064d1
LuisMasluCan <lmaslucancanyo@gmail.com>
2025-08-28 00:11:02 -0500

    update-text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,15 @@ export default function Home() {
       <>
           <Navbar />
           <Slide />
+          <section className="max-w-7xl mx-auto px-4 mb-6">
+              <h2 className="text-center text-2xl font-extrabold tracking-wide">
+                  <span className="text-orange-500">PRODUCTOS</span> DESTACADOS
+              </h2>
+              <p className="text-center text-gray-400 mt-1 font-semibold tracking-wide">
+                  Nuestro mejores productos
+              </p>
+          </section>
+
           <div className="max-w-7xl mx-auto p-4">
               <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4">
                   <ProductCard />

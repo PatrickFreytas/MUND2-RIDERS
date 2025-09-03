ad9f2a82c31c6da36a778bf0c196d905eb0b7bfd
PatrickFreytas <135546208+PatrickFreytas@users.noreply.github.com>
2025-09-03 06:38:09 -0500

    feat: fix layout

--- src/app/[subdomain]/home/layout.tsx
+++ src/app/[subdomain]/home/layout.tsx
@@ -1,15 +1,15 @@
+import CategoriesLoader from "@/category/components/categories-loader";
+import {CategoryStoreProvider} from "@/category/components/category-store-provider";
+import React from "react";
+
 export default function HomeLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>
-        <header></header>
-          <main>{children}</main>
-        <footer></footer>
-      </body>
-    </html>
+    <>
+      {children}
+    </>
   );
 }

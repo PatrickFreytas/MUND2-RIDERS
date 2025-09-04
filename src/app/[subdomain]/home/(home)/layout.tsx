c109e43a6c4f07e7ee8637c0cb546cd46f5b039d
PatrickFreytas <135546208+PatrickFreytas@users.noreply.github.com>
2025-09-04 14:14:01 -0500

    feat: modify header for landing page

--- src/app/[subdomain]/home/(home)/layout.tsx
+++ src/app/[subdomain]/home/(home)/layout.tsx
@@ -2,12 +2,14 @@ import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
 import { extractRouterConfig } from "uploadthing/server";
 import { ourFileRouter } from "@/app/api/uploadthing/core";
 import {ScrollArea} from "@/shared/components/ui/scroll-area";
+import Header from "@/shared/components/layout-landing/header";
 
 export default function DashboardLayout({
   children,
 }: {children: React.ReactNode}) {
   return (
     <>
+      <Header/>
       <div className="flex h-screen overflow-hidden">
         <main className="w-full pt-14">
           <ScrollArea className="h-full">

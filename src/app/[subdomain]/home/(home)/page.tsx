7881a29928a381942a8f0d471aac4c478003a649
PatrickFreytas <135546208+PatrickFreytas@users.noreply.github.com>
2025-09-04 18:03:24 -0500

    improve: add product list for landing page

--- src/app/[subdomain]/home/(home)/page.tsx
+++ src/app/[subdomain]/home/(home)/page.tsx
@@ -1,14 +1,14 @@
 import Footer from "@/shared/footer/footer";
 import Slide from "@/shared/slide/slide";
 import Brand from "@/shared/brand/brand";
-import ProductCard from "@/product/components/product-view-landing/product-card";
+import ProductList from "@/shared/components/layout-landing/product-list";
 
 
 export default function HomePage() {
   return (
     <>
-      <Slide />
-      <Brand />
+      <Slide/>
+      <Brand/>
       <section className="max-w-7xl mx-auto px-4 mb-6">
         <h2 className="text-center text-2xl font-extrabold tracking-wide">
           <span className="text-orange-500">PRODUCTOS</span> DESTACADOS
@@ -17,13 +17,8 @@ export default function HomePage() {
           Nuestro mejores productos
         </p>
       </section>
-
-      <div className="max-w-7xl mx-auto p-4">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4">
-          <ProductCard />
-        </div>
-      </div>
-      <Footer />
+      <ProductList/>
+      <Footer/>
     </>
   );
 }
--- src/product/components/product-view-landing/product-card.tsx
+++ src/product/components/product-view-landing/product-card.tsx
@@ -3,69 +3,37 @@
 import { Card, CardContent } from "@/shared/components/ui/card";
 import Image from "next/image";
 import {FaWhatsapp} from "react-icons/fa";
-import {useProductFormActions, useProductFormStore} from "@/product/components/product-view-landing/product-searcher-form-provider";
-import {SortOptions} from "@/product/types";
-import {useEffect, useState} from "react";
-import {getMany, GetManyParams} from "@/product/api_repository";
-import {toast} from "@/shared/components/ui/use-toast";
+import {Product} from "@/product/types";
 import {formatPrice} from "@/lib/utils";
 
-export default function ProductCard() {
-  const { setProducts } = useProductFormActions();
-  const products = useProductFormStore((store) => store.products);
+export default function ProductCard({ product }: { product: Product }) {
 
-  const searchProduct = async () => {
-    const response = await getMany();
-
-    if (!response.success) {
-      toast({
-        title: "Error",
-        variant: "destructive",
-        description: response.message,
-      });
-      return;
-    }
-
-    setProducts(response.data);
-  };
-
-  useEffect(() => {
-    searchProduct();
-  }, []);
   return (
-    <div className="relative">
-      {products.length ? (
-        products.map((product) => (
-          <Card
-            key={product.id}
-            className="w-full max-w-[260px] rounded-xl border shadow-sm hover:shadow-md transition-all cursor-pointer">
-            <CardContent className="p-4 flex flex-col items-center">
-              <div className="relative w-[120px] h-[120px] mb-2">
-                <Image fill className="object-cover" alt="Image" src={product.photos?.[0]?.url || ""}
-                />
-              </div>
-              <p className="text-sm text-center font-medium leading-tight line-clamp-2">
-                {product.name}
-              </p>
-              <p className="text-lg font-semibold text-center text-black my-1">
-                {formatPrice(product.price)}
-              </p>
-              <p className="text-sm text-center text-gray-600">
-                {product.categories.map((category) => (category.name === "KTM") ? "KTM" : "")}
-              </p>
-              <button
-                className="mt-3 inline-flex items-center gap-2 text-white bg-green-500 hover:bg-green-600 px-4 py-1.5 rounded-md text-sm"
-                onClick={() => window.open("https://wa.me/51999999999", "_blank")}
-              >
-                <FaWhatsapp className="text-lg"/>
-                Comprar
-              </button>
-            </CardContent>
-          </Card>
-        ))
-      ) : (
-        <div className="text-center">No hay productos</div>
-      )}
-    </div>
+    <Card
+      key={product.id}
+      className="w-full max-w-[260px] rounded-xl border shadow-sm hover:shadow-md transition-all cursor-pointer">
+      <CardContent className="p-4 flex flex-col items-center">
+        <div className="relative w-[120px] h-[120px] mb-2">
+          <Image fill className="object-cover" alt="Image" src={product.photos?.[0]?.url || ""}
+          />
+        </div>
+        <p className="text-sm text-center font-medium leading-tight line-clamp-2">
+          {product.name}
+        </p>
+        <p className="text-lg font-semibold text-center text-black my-1">
+          {formatPrice(product.price)}
+        </p>
+        <p className="text-sm text-center text-gray-600">
+          {product.categories.map((category) => (category.name === "KTM") ? "KTM" : "")}
+        </p>
+        <button
+          className="mt-3 inline-flex items-center gap-2 text-white bg-green-500 hover:bg-green-600 px-4 py-1.5 rounded-md text-sm"
+          onClick={() => window.open("https://wa.me/51999999999", "_blank")}
+        >
+          <FaWhatsapp className="text-lg"/>
+          Comprar
+        </button>
+      </CardContent>
+    </Card>
   )
 }

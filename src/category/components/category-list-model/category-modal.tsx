e8b2c4f1188559d85f1bfb3b6a9e749ac51c24fc
PatrickFreytas <135546208+PatrickFreytas@users.noreply.github.com>
2025-09-05 17:22:15 -0500

    feat: fix bug in the view

--- src/category/components/category-list-model/category-modal.tsx
+++ src/category/components/category-list-model/category-modal.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { Dialog, DialogContent, DialogTrigger } from "@/shared/components/ui/dialog";
+import {Dialog, DialogContent, DialogTitle, DialogTrigger} from "@/shared/components/ui/dialog";
 import { Button } from "@/shared/components/ui/button";
 import { useCategoryStore } from "@/category/components/category-store-provider";
 import CategoryContent from "./category-content";
@@ -9,6 +9,7 @@ import NewCategoryDialog from "@/product/components/category/new-category-dialog
 import {Category} from "@/category/types";
 import React from "react";
 import { List } from 'lucide-react';
+import {VisuallyHidden} from "@radix-ui/react-visually-hidden";
 
 interface NewSectionDialogProps {
   addCategory: (category: Category) => void;
@@ -32,29 +33,37 @@ export default function CategoriesModal({
         </Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-96 sm:h-[429.5] w-full flex flex-col items-center p-0 ">
-        <table className="min-w-96 rounded-2xl">
-          <thead>
-          <tr className="bg-gray-100 text-gray-600 uppercase text-sm leading-normal">
-            <th className="py-3 px-6 text-left">Categoria</th>
-          </tr>
-          </thead>
-          <tbody className="text-sm">
-          <ScrollArea className="h-96">
+        <VisuallyHidden>
+          <DialogTitle>Formulario de Categoría</DialogTitle>
+        </VisuallyHidden>
+        <ScrollArea className="h-96">
+          <table className="min-w-96 rounded-2xl">
+            <thead>
+            <tr className="bg-gray-100 text-gray-600 uppercase text-sm leading-normal">
+              <th className="py-3 px-6 text-left">Categoria</th>
+            </tr>
+            </thead>
+            <tbody className="text-sm">
             {categories.length ? (
               categories.map(category => (
-                <CategoryContent key={category.id} category={category}
-                                 onCategoryUpdated={(category) => updateCategory(category)}/>
+                <CategoryContent
+                  key={category.id}
+                  category={category}
+                  onCategoryUpdated={updateCategory}
+                />
               ))
             ) : (
-              <tr className=" text-black">
+              <tr className="text-black">
                 <td className="h-96">
-                  <div className="flex items-center justify-center h-full">No hay categorias</div>
+                  <div className="flex items-center justify-center h-full">
+                    No hay categorias
+                  </div>
                 </td>
               </tr>
             )}
-          </ScrollArea>
-          </tbody>
-        </table>
+            </tbody>
+          </table>
+        </ScrollArea>
         <NewCategoryDialog addCategory={addCategory}/>
       </DialogContent>
     </Dialog>
